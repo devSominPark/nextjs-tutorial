3e1d6e34db083b084178a5810700e145c3fa9de3
devSominPark <min85472@gmail.com>
2021-11-25 00:58:00 +0900

    ServerSideRendering 적용 전

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
+import Footer from "../src/component/Footer";
+import Top from "../src/component/Top";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // Component : 현재 페이지를 의미, 페이지 전환시 Component의 props가 변경
+  // pageProps : data patching method를 통해 미리 가져온 초기 객체
+
+  return (
+    <div style={{ width: "60%", margin: "0 auto" }}>
+      <Top />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
