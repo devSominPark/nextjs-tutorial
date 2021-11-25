6709908a97a59b2471708c6c1161ec950bfe6f8c
devSominPark <min85472@gmail.com>
2021-11-25 21:02:20 +0900

    isFallback 적용

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,8 +1,23 @@
 import axios from "axios";
 import Item from "../../src/component/Item";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { Loader } from "semantic-ui-react";
 
 const Post = ({ item }) => {
+  const router = useRouter();
+  // router.isFallback : 화면이 그려지기 전엔 true, 그려진 후 false
+
+  if (router.isFallback) {
+    return (
+      <div style={{ padding: "100px 0" }}>
+        <Loader active inline="centered">
+          Loading
+        </Loader>
+      </div>
+    );
+  }
+
   return (
     <>
       {item && (
@@ -21,18 +36,22 @@ const Post = ({ item }) => {
 export default Post;
 
 export async function getStaticPaths() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
   return {
-    paths: [
-      { params: { id: "740" } },
-      { params: { id: "730" } },
-      { params: { id: "729" } },
-      ,
-    ],
+    paths: data.slice(0, 9).map((item) => ({
+      params: {
+        id: item.id.toString(),
+      },
+    })),
     fallback: true,
     // false : 없는 페이지는 대응해주지 않음(404)
     // true : 없는 페이지일 경우 처음엔 props가 빈 상태로 그려지다
     // 백그라운드에서 정적파일로 HTML과 JSON을 생성한 후 NextJS는 프리렌더링 목록에 추가
     // 두번째 접속부터 정적페이지 파일을 사용
+    // 첫 접속시 빈 페이지가 나오는 것을 router.isFallback을 활용해 로딩중을 띄워줄 수 있음
   };
 }
 
